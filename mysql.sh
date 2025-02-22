1bddbc2fa50d71e87e8f02ad116b5e29dc8a67c8
nnreddybalreddy <narendra.mahn@gmail.com>
2025-02-21 21:40:50 -0500

    ALL

--- mysql.sh
+++ mysql.sh
@@ -1,35 +1,5 @@
 #!/bin/bash
-TIMESTAMP=$(date +%F-%M-%H-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-echo "Enter password"
-read mysql_root_password
-
-USERID=$(id -u)
-
-if [ $USERID -eq  0 ]
-then
-    echo "You are super"
-else 
-    echo "Not a super user"
-    exit 1    
-fi
-
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then 
-        echo -e "$2 $G success $N"
-    else 
-        echo -e "$2 $G fail $N"
-        exit 1     
-    fi    
-}
+source ./common.sh
 
 # VALIDATE(){
 #     if [ $? -eq 0 ]
