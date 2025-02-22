8bc0a58542da39e5527d1b818718b19b504e37a1
nnreddybalreddy <narendra.mahn@gmail.com>
2025-02-21 21:26:18 -0500

    ALL

--- mysql.sh
+++ mysql.sh
@@ -18,33 +18,33 @@ else
     exit 1    
 fi
 
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then 
-        echo -e "$G $2 is success $N"
-    else    
-        echo -e "$G $2 is fail $N"
-}
+# VALIDATE(){
+#     if [ $1 -eq 0 ]
+#     then 
+#         echo -e "$G $2 is success $N"
+#     else    
+#         echo -e "$G $2 is fail $N"
+# }
 
 
-dnf install mysql-server -y &>>$LOGFILE
-VALIDATE $? "mysql server installation"
+# dnf install mysql-server -y &>>$LOGFILE
+# VALIDATE $? "mysql server installation"
 
 
-systemctl enable mysqld &>>$LOGFILE
-VALIDATE $? "enable mysqld"
+# systemctl enable mysqld &>>$LOGFILE
+# VALIDATE $? "enable mysqld"
 
-systemctl start mysqld &>>$LOGFILE
-VALIDATE $? "start mysqld"
+# systemctl start mysqld &>>$LOGFILE
+# VALIDATE $? "start mysqld"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-VALIDATE $? "password set "
+# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+# VALIDATE $? "password set "
 
-mysql -h db.narendra.shop -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+# mysql -h db.narendra.shop -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 
-if [ $? -ne 0 ]
-then
-    mysql_secure_installation --set-root-pass ${mysql_root_password}  &>>$LOGFILE
-else
-    echo -e "$Y Password alrady set $N"
-fi
+# if [ $? -ne 0 ]
+# then
+#     mysql_secure_installation --set-root-pass ${mysql_root_password}  &>>$LOGFILE
+# else
+#     echo -e "$Y Password alrady set $N"
+# fi
